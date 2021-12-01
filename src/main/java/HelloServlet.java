d078fbe60444c10f9bd6fac9dad290f7ebeb14d6
Rodriques <rodriques.perry@gmail.com>
2021-12-01 10:58:23 -0600

    lecture complete

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/helloWorld")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = request.getParameter("name");
+        // if the name is null, it means that "name" was not present in the query
+        // string, and here we'll provide a default value
+        if (name == null) {
+            name = "World!";
+        } else if (name.equals("bgates")) {
+            response.sendRedirect("https://microsoft.com");
+            return;
+        }
+        // pass the value of the name variable to the view, and send the request
+        // forward to the hello.jsp file
+        request.setAttribute("name", name);
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rodriquesperry
+  Date: 12/1/21
+  Time: 10:49 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>Hello there, ${name}!</h1>
+</body>
+</html>
