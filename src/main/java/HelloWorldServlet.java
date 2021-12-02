87fbc8e788b8ac461d6b6afded05c376067858bb
Rodriques <rodriques.perry@gmail.com>
2021-12-01 22:29:49 -0600

    pushing to correct branch

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -8,12 +8,6 @@ import java.io.PrintWriter;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-    private int hitCount;
-
-    public void init() {
-        hitCount = 0;
-    }
-
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
             res.setContentType("text/html");
@@ -21,18 +15,16 @@ public class HelloWorldServlet extends HttpServlet {
             String title = "Total Number of Hits";
             PrintWriter out = res.getWriter();
 
-            hitCount++;
+
 
 
             if (searchQuery != null) {
                 out.println("<h1>Hello, " + searchQuery + "!</h1>");
-                hitCount = 0;
+
             } else {
                 out.println("<h1>Hello World!</h1>");
             }
 
-
-            out.println(title + "\n<h3>" + hitCount + "</h3>");
         }
 
 }
